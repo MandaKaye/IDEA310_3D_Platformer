2f729b1737e881e9cc79956c7d5df2907ef8f6f1
MandaKaye <mandakaye320@gmail.com>
2026-03-11 21:16:12 -0600

    Level One done maybe
    Added more turtles and added menu scene to scenes build out. Collection of five turtles sends to Menu scene

--- Assets/Scripts/TurtleCollecter.cs
+++ Assets/Scripts/TurtleCollecter.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class TurtleCollecter : MonoBehaviour
 {
     public TMP_Text TurtleText;
+    public int NumTurtsInLvl = 5;
+    public string LvltoLoad = "Menu";
+
     private int TurtlesCollected = 0;
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,6 +18,11 @@ public class TurtleCollecter : MonoBehaviour
             TurtlesCollected += 1;
             TurtleText.text = "Turtles Collected: " + TurtlesCollected;
             Destroy(other.gameObject);
+
+            if(TurtlesCollected == NumTurtsInLvl)
+            {
+                SceneManager.LoadScene("Menu");
+            }
        } 
     }
 }
